b48cc83411fa833b6ea5114dbdcb4274991a8996
Odusanya Victor Olukayode <Odusanya18@gmail.com>
2019-11-05 23:18:42 +0100

    removed curse word

--- src/main/java/example/smallest/controllers/WelcomeController.java
+++ src/main/java/example/smallest/controllers/WelcomeController.java
@@ -16,6 +16,6 @@ public class WelcomeController {
 		
 		
 		
-		return "Hello FUCKING World!!!"; //"application/json" mean this is a text not a redirect
+		return "Hello World from Spring Boot!!!"; //"application/json" mean this is a text not a redirect
 	}
 }
